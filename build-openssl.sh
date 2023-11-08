c4ae70661ab4049db5ab40d4bd2e46b9c320830a
Julia Nelz <121945980+I-asked@users.noreply.github.com>
2023-11-08 12:12:09 +0100

    Fix building OpenSSL

--- build-openssl.sh
+++ build-openssl.sh
@@ -7,7 +7,7 @@ set -x
   && git clone --depth 1 --branch OpenSSL_0_9_8-stable --single-branch https://github.com/openssl/openssl.git openssl/
 if [[ ! -f openssl/libcrypto.a ]] ; then
   pushd openssl/
-  ./Configure 'linux-elf:gcc -m32'
+  ./Configure no-shared no-dso 386 'linux-generic32:gcc -m32 -Werror=undef -DHAVE_LONG_LONG=1'
   make -j"$(nproc)" build_crypto
   popd
 fi
