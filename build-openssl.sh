02739c210e6c28689179ad122e377948fcc6df99
Julia Nelz <121945980+I-asked@users.noreply.github.com>
2023-11-08 17:38:41 +0100

    Use released OpenSSL

--- build-openssl.sh
+++ build-openssl.sh
@@ -3,11 +3,11 @@ set -Eeuo pipefail
 
 set -x
 
-[[ ! -d openssl/ ]] \
-  && git clone --depth 1 --branch OpenSSL_0_9_8-stable --single-branch https://github.com/openssl/openssl.git openssl/
-if [[ ! -f openssl/libcrypto.a ]] ; then
-  pushd openssl/
-  ./Configure no-shared no-dso 386 'linux-generic32:gcc -m32 -Werror=undef -DHAVE_LONG_LONG=1'
-  make -j"$(nproc)" build_crypto
-  popd
-fi
+[[ -d openssl/ ]] \
+  && rm -rf openssl/
+mkdir openssl/
+>&2 pushd openssl/
+wget -O- https://ftp.openssl.org/source/old/0.9.x/openssl-0.9.8zh.tar.gz |tar --strip-components=1 -xzf-
+>&2 ./Configure no-shared no-dso 386 'linux-generic32:gcc -m32 -Werror=undef -Werror -DHAVE_LONG_LONG=1'
+>&2 make -j"$(nproc)" build_crypto
+>&2 popd
