ecaefc39cc59a8ecfa77bd7ce6bc4d42239d983c
Julia Nelz <121945980+I-asked@users.noreply.github.com>
2023-11-10 09:17:28 +0100

    Minor improvements to build-openssl.sh

--- build-openssl.sh
+++ build-openssl.sh
@@ -6,8 +6,8 @@ set -x
 [[ -d openssl/ ]] \
   && rm -rf openssl/
 mkdir openssl/
->&2 pushd openssl/
+>&/dev/null pushd openssl/
 wget -O- https://ftp.openssl.org/source/old/0.9.x/openssl-0.9.8zh.tar.gz |tar --strip-components=1 -xzf-
->&2 ./Configure no-shared no-dso 386 'linux-generic32:gcc -m32 -Werror=undef -Werror -DHAVE_LONG_LONG=1'
->&2 make -j"$(nproc)" build_crypto
->&2 popd
+./Configure no-shared no-dso 386 'linux-generic32:gcc -m32 -Werror=undef -Werror -DHAVE_LONG_LONG=1'
+make -j"${MAKEJOBS}" ${MAKEFLAGS} build_crypto
+>&/dev/null popd
